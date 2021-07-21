ce4543e5935a498bb281d46b6cd701f166ce693d
Javier Chireno <javierchireno04@gmail.com>
2021-07-21 13:36:57 -0400

    commit tree 567d45b2aee7455afdff8c2f188acd9c34f6708b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+function saturdayFun(s = 'roller-skate'){
+       return `This Saturday, I want to ${s}!`   
+}
+
+function mondayWork(m = 'go to the office'){
+    return `This Monday, I will ${m}.`
+}
+
+function wrapAdjective( n= '*' , ){
+   return function ( emphatic ='a hard worker'){
+       return `You are ${n}${emphatic}${n}!`
+
+   } 
+} 
+saturdayFun();
+mondayWork();
+wrapAdjective('||')("dedicated programmer");
+console.log(saturdayFun('bathe my dog'),mondayWork('work from home'));
